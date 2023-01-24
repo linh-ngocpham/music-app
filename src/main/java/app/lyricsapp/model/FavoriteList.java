f0afe936408aaa47babf4f2f38c4d664c484069a
cnc cnc <djibril.mimouni@gmail.com>
2023-01-24 16:36:12 +0100

    FavoriteList class

---
+++ src/main/java/app/lyricsapp/model/FavoriteList.java
@@ -0,0 +1,20 @@
+package app.lyricsapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FavoriteList {
+    private Set<Integer> favoritesSongsID;
+
+    public FavoriteList(){
+        this.favoritesSongsID = new HashSet<>();
+    }
+
+    public void add(Song song){
+        this.favoritesSongsID.add(song.getLyricId());
+    }
+
+    public void remove(Song song){
+        this.favoritesSongsID.remove(song.getLyricId());
+    }
+}
